0df26634ed71b2d565e77a0e461128e031f713d5
CG <c@g.edu>
2019-11-15 15:03:12 -0500

    Added js for LibCal front page, adds * to Sats

---
+++ LibCal/appendAsteriskToLibCal.js
@@ -0,0 +1,16 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+$(document).ready(function(){
+
+// Append an asterisk to each <td> element that has Saturday hours
+$("tr.s-lc-mhw-cells td:nth-child(6) .s-lc-time").append("<span style='font-size: 1.3em; color: #c41230;'>*</span>");
+  
+  
+})
