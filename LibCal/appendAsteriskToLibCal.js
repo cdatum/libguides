2225a670da494e95446708404a7ee45781f09348
Galluzzo <cgalluzzo@ccac.org>
2019-11-25 14:00:42 -0500

    Updates based on calendar column layout change

--- LibCal/appendAsteriskToLibCal.js
+++ LibCal/appendAsteriskToLibCal.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
 // Append an asterisk to each <td> element that has Saturday hours
-    $("tr.s-lc-mhw-cells > td:nth-child(6) > div.loc_7958 > span.s-lc-time").append("<span style='font-size: 1.3em; color: #c41230;'>*</span>");
+$("tr.s-lc-mhw-cells  td:nth-child(7) div.s-lc-mhw-loc span.s-lc-time").append("<span style='font-size: 1.3em; color: #c41230;'>*</span>");
 });
