b3edd83d325da14af078dc8da32ff85c77316da1
CG <cgalluzzo@ccac.edu>
2021-06-14 13:30:40 -0400

    wrapped code in a document.ready()

--- CustomDescription/descHyperlink.js
+++ CustomDescription/descHyperlink.js
@@ -1,13 +1,17 @@
-/* 
+/*
 This script is to accompnay the code created by the Libguides Custom Description tool.
-It finds thumbnail images created by the tool and then locates the URL associated with 
+It finds thumbnail images created by the tool and then locates the URL associated with
 the database/link. Finally, the image is converted into a link.
+
+For use in LibGuides, this needs to be wrapped in a <script>
  */
 
+$(document).ready(function(){
+  var img = $('.s-lg-link-desc, .s-lg-database-desc').find('img');
+  // Find URL for db and wrap <img> in an <a href> set to the URL
+  $.each(img, function(){
+  var url = $(this).closest('li').find('a').attr('href');
+  $(this).wrap("<a target=\"_blank\" href='" + url + "'></a>")
+  });
 
-var img = $('.s-lg-link-desc, .s-lg-database-desc').find('img');
-// Find URL for db and wrap <img> in an <a href> set to the URL
-$.each(img, function(){
-var url = $(this).closest('li').find('a').attr('href');
-$(this).wrap("<a target=\"_blank\" href='" + url + "'></a>")
 });
